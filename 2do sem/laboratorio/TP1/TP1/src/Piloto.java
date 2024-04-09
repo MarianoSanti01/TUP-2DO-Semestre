455c29bfdad3dd137cabc1d0fc0a105eeaad2c46
Mariano Santi <marianosanti2014@gmail.com>
2024-04-09 11:24:18 -0300

    09/04

--- 2do sem/laboratorio/TP1/TP1/src/Piloto.java
+++ 2do sem/laboratorio/TP1/TP1/src/Piloto.java
@@ -1,4 +1,23 @@
-public class Piloto extends Persona implements Volador {
-    public void volar() {
+package SistReserva;
+
+public class Piloto extends Persona {
+
+    private String carnetPiloto;
+
+    public Piloto() {
+        super(); // Call to the constructor of the superclass (Persona)
+    }
+
+    public Piloto(String nombre, String apellido, int edad, int dni, String carnetPiloto) {
+        super(nombre, apellido, edad, dni); // Call to the constructor of the superclass (Persona) to set common attributes
+        this.carnetPiloto = carnetPiloto;
+    }
+
+    public String getCarnetPiloto() {
+        return carnetPiloto;
+    }
+
+    public void setCarnetPiloto(String carnetPiloto) {
+        this.carnetPiloto = carnetPiloto;
     }
 }
